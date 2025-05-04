104e09666c4b1630426f48475d5b4efaec12da51
Tài Khương <khuonganhtai05@gmail.com>
2025-05-04 16:43:01 +0700

    Create a new branch for homework

---
+++ app/src/main/java/hust/kat/studentadder/AddActivity.kt
@@ -0,0 +1,13 @@
+package hust.kat.studentadder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class AddActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
--- app/src/main/java/hust/kat/studentadder/StudentModel.kt
+++ app/src/main/java/hust/kat/studentadder/StudentModel.kt
@@ -5,4 +5,6 @@ import android.widget.Button
 data class StudentModel(
     val fullName: String,
     val studentID: String,
+    private val phoneNumber: Number,
+    private val email: String
 )
